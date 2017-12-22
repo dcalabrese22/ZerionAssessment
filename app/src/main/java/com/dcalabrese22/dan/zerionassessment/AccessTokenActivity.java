cf3482afadcf88ead7426920fb34ff40a9bc0804
dcalabrese22 <dcalabrese22@gmail.com>
2017-12-22 14:40:07 -0500

    create jwt and get token from server

--- app/src/main/java/com/dcalabrese22/dan/zerionassessment/AccessTokenActivity.java
+++ app/src/main/java/com/dcalabrese22/dan/zerionassessment/AccessTokenActivity.java
@@ -1,8 +1,7 @@
 package com.dcalabrese22.dan.zerionassessment;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 
 public class AccessTokenActivity extends AppCompatActivity {
 
@@ -11,6 +10,7 @@ public class AccessTokenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_access_token);
 
-        Log.d("Sig", AccessTokenGenerator.createToken("3e5065eb19bdf3ca90cc1fea582b758103cda27f"));
+        String token = AccessTokenGenerator.createJwt(Constants.ISSUER);
+        MyRequestQueue.getInstance(this).addToRequestQueue(AccessTokenGenerator.getAccessToken(token));
     }
 }
--- app/src/main/java/com/dcalabrese22/dan/zerionassessment/AccessTokenGenerator.java
+++ app/src/main/java/com/dcalabrese22/dan/zerionassessment/AccessTokenGenerator.java
@@ -3,12 +3,13 @@ package com.dcalabrese22.dan.zerionassessment;
 import android.util.Base64;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
 import org.json.JSONException;
 import org.json.JSONObject;
-
-
-import java.security.PrivateKey;
-import java.security.Signature;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
@@ -19,42 +20,112 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class AccessTokenGenerator {
 
+    private static final String HEADER = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}";
+    private static final String ENCODED_HEADER = Base64.encodeToString(HEADER.getBytes(),
+            Base64.NO_WRAP |Base64.URL_SAFE);
 
-    public static String createToken(String iss) {
-        String header = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}";
-        String encodedHeader = Base64.encodeToString(header.getBytes(), Base64.DEFAULT);
-        Log.d("Header", encodedHeader);
+
+    public static String createJwt(String iss) {
 
         long timeStamp = System.currentTimeMillis()/1000;
+        long timePlus5min = timeStamp + 300;
 
-        JSONObject objPayload = new JSONObject();
-        Log.d("timestamp", String.valueOf(timeStamp));
+        Log.d("Timestamp", String.valueOf(timeStamp));
+        Log.d("time plus 5 ", String.valueOf(timePlus5min));
+
+        String payload = "{\"iss\":\"" + iss + "\"," +
+                "\"aud\":\"https://app.iformbuilder.com/exzact/api/oauth/token\"," +
+                "\"exp\":" + timePlus5min + "," +
+                "\"iat\":" + timeStamp + "}";
+
+        String encodedPayload = Base64.encodeToString(payload.getBytes(),
+                Base64.NO_WRAP | Base64.URL_SAFE);
+        encodedPayload = encodedPayload.replace("=", "");
+
         try {
-            objPayload.put("iss", iss);
-            objPayload.put("aud", "https://app.iformbuilder.com/exzact/api/oauth/token");
-            objPayload.put("exp", timeStamp + 300);
-            objPayload.put("iat", timeStamp);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        String encodedPayload = Base64.encodeToString(objPayload.toString().getBytes(),
-                Base64.DEFAULT);
-        Log.d("Payload", encodedPayload);
+            Mac mac = Mac.getInstance("HmacSHA256");
+            String comb = ENCODED_HEADER + "." + encodedPayload;
+            SecretKeySpec secretKeySpec = new SecretKeySpec(Constants.SECRET_KEY
+                    .getBytes("UTF-8"), "HmacSHA256");
+            mac.init(secretKeySpec);
 
-        String secret = "35a17e4ff49f8b66f04e63edd3449052ae72e7e2";
-        try {
-            Mac sha256 = Mac.getInstance("HmacSHA256");
-            String comb = encodedHeader + "." + encodedPayload;
-            Log.d("combined", comb);
-            SecretKeySpec secretKeySpec = new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256");
-            sha256.init(secretKeySpec);
-            byte[] sig = sha256.doFinal(comb.getBytes("UTF-8"));
+            String hash = Base64.encodeToString(mac.doFinal(comb.getBytes()),
+                    Base64.URL_SAFE | Base64.NO_WRAP);
+            hash = hash.replace("=", "");
 
+            String jwt = comb + "." + hash;
+            jwt = jwt.replace("\n", "");
+            Log.d("JWT", jwt);
+            return jwt;
 
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
+
+    public static JsonObjectRequest getAccessToken(final String jwt) {
+        final String grantTypeString = "&grant_type=";
+        final String grantType = "urn:ietf:params:oauth:grant-type:jwt-bearer";
+        final String grantTypeUrlEncode = "urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer";
+        final String assertion = "&assertion=";
+        String url = "https://app.iformbuilder.com/exzact/api/oauth/token";
+        final StringBuilder accessToken = new StringBuilder();
+
+        try {
+            JSONObject object = new JSONObject();
+            object.put("grant_type", grantType);
+            object.put("assertions", jwt);
+            final JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url,
+                    object,new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        accessToken.append(response.getString("access_token"));
+                        Log.d("Token", accessToken.toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                }
+            }) {
+
+                @Override
+                public String getBodyContentType() {
+                    return "application/x-www-form-urlencoded; charset=UTF-8";
+                }
+
+                @Override
+                public byte[] getBody() {
+                    Log.d("getBody", "called");
+                    String body = grantTypeString + grantTypeUrlEncode +
+                          assertion + jwt;
+                    return body.getBytes();
+                }
+            };
+
+            return request;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+//    public static void makeRequest(String jwt) {
+//        try {
+//            URL url = new URL("https://app.iformbuilder.com/exzact/api/oauth/token");
+//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//            connection.setRequestMethod("POST");
+//            connection.setRequestProperty("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer");
+//            connection.setRequestProperty("assertion", jwt);
+//
+//        }
+//    }
+
+
 
 }
